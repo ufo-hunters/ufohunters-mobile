7ea2b496ffc603f0aafc674b26fe2de721b10e6d
José Carlos García <jcarlosgarcia@hotmail.com>
2013-07-23 23:55:59 +0200

    Geolocation plugin test

---
+++ assets/www/js/map.js
@@ -0,0 +1,29 @@
+$('#mapPage').live('pageshow', function(event) {
+  displaySightings();
+});
+
+function displaySightings() {
+    
+	$.getJSON(serviceURL + 'reports.json', function(data) {
+	     $('#sightingsList li').remove();
+	     $.each(data, function(key, val) {
+		     var sightingDate = val.sighted_at;
+		     if (sightingDate.length >= 8) {
+		    	 var year = sightingDate.substring(0,4);
+		    	 var month = sightingDate.substring(4,6);
+		    	 var day = sightingDate.substring(6,8);
+		    	 var date = new Date(year, month-1, day);					
+		    	 sightingDate = $.datepicker.formatDate("DD d. MM yy", date);		     
+		     } else {
+		    	 sightingDate = "N/A";
+		     }
+		     
+		     $('#sightingsList').append('<li><a href="report.html?id=' + 
+		      val._id + '"><h4>' + val.location + 
+		      '</h4><p>' + sightingDate + '</p></a>' + 
+		      '</li>' 
+		     );
+		  });		
+		$('#sightingsList').listview('refresh');
+	});
+}
