3f588da37af1d4e0d8b49149afff6015058bffa4
fjfdepedro <fjfdepedro@gmail.com>
2013-09-21 12:15:52 +0200

    texts

--- assets/www/js/list.js
+++ assets/www/js/list.js
@@ -26,37 +26,42 @@ function getSightingsList() {
     //$.getJSON(serviceURL + 'reports.json', function(data) {
     function showInfo(data) {	
 	     $('#sightingsList li').remove();
+	     var count = 1;
 	     $.each(data, function(key, val) {
-		     var sightingDate = val.sighted_at;
-			 var sightingDay;
-			 var sightingMonth; 
-			 var sightingYear; 
-		     if (sightingDate.length >= 8) {
-		    	 var year = sightingDate.substring(0,4);
-		    	 var month = sightingDate.substring(4,6);
-		    	 var day = sightingDate.substring(6,8);
-		    	 var date = new Date(year, month-1, day);					
-		    	 sightingDate = $.datepicker.formatDate("DD d. MM yy", date);	
-				 sightingDay = $.datepicker.formatDate("d", date);
-				 sightingMonth = $.datepicker.formatDate("M", date).toUpperCase();	
-				 sightingYear = $.datepicker.formatDate("yy", date);					 
-		     } else {
-		    	 sightingDate = "N/A";
-		     }
-		     var reportedDate = val.reported_at;
-		     if (reportedDate.length >= 8) {
-		    	 var yearR = reportedDate.substring(0,4);
-		    	 var monthR = reportedDate.substring(4,6);
-		    	 var dayR = reportedDate.substring(6,8);
-		    	 var dateR = new Date(yearR, monthR-1, dayR);					
-		    	 reportedDate = $.datepicker.formatDate("DD d. MM yy", dateR);		 
-		     } else {
-		    	 reportedDate = "N/A";
-		     }
-		     var sightingShape = val.shape;
-		     var sightingSource = val.source;
-		     $('#sightingsList').append('<li><a href="report.html?id=' + val._id + '"><strong><em>' + sightingDay + '</em><i class="' + sightingMonth + '">' + sightingMonth + '</i><b class="' + sightingMonth + '">' + sightingYear + '</b></strong><b>' + val.location + '</b><p><u>Reported on:</u>' + reportedDate + '</p><p><u>Shape:</u>' + sightingShape + ' | <u>Source:</u>' + sightingSource + '</p></li>');
-			 
+
+	     		if(count<11){
+				     var sightingDate = val.sighted_at;
+					 var sightingDay;
+					 var sightingMonth; 
+					 var sightingYear; 
+				     if (sightingDate.length >= 8) {
+				    	 var year = sightingDate.substring(0,4);
+				    	 var month = sightingDate.substring(4,6);
+				    	 var day = sightingDate.substring(6,8);
+				    	 var date = new Date(year, month-1, day);					
+				    	 sightingDate = $.datepicker.formatDate("DD d. MM yy", date);	
+						 sightingDay = $.datepicker.formatDate("d", date);
+						 sightingMonth = $.datepicker.formatDate("M", date).toUpperCase();	
+						 sightingYear = $.datepicker.formatDate("yy", date);					 
+				     } else {
+				    	 sightingDate = "N/A";
+				     }
+				     var reportedDate = val.reported_at;
+				     if (reportedDate.length >= 8) {
+				    	 var yearR = reportedDate.substring(0,4);
+				    	 var monthR = reportedDate.substring(4,6);
+				    	 var dayR = reportedDate.substring(6,8);
+				    	 var dateR = new Date(yearR, monthR-1, dayR);					
+				    	 reportedDate = $.datepicker.formatDate("DD d. MM yy", dateR);		 
+				     } else {
+				    	 reportedDate = "N/A";
+				     }
+				     var sightingShape = val.shape;
+				     var sightingSource = val.source;
+				     $('#sightingsList').append('<li><a href="report.html?id=' + val._id + '"><strong><em>' + sightingDay + '</em><i class="' + sightingMonth + '">' + sightingMonth + '</i><b class="' + sightingMonth + '">' + sightingYear + '</b></strong><b>' + val.location + '</b><p><u>Reported on:</u>' + reportedDate + '</p><p><u>Shape:</u>' + sightingShape + ' | <u>Source:</u>' + sightingSource + '</p></li>');
+				     count++;
+			    }
+
 		  });		
 		$('#sightingsList').listview('refresh');
 	}		
